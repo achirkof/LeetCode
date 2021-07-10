6e9dbbd3a6964eb70d75410fa62588844ff16481
Andrey Chirkov <achirkof@gmail.com>
2021-07-10 20:54:15 +0300

    Implement StrStr simplest and fastest way

---
+++ LeetCode.playground/Pages/14 - LongestCommonPrefix.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+ import Foundation
+
+/*
+ Input: strs = ["flower","flow","flight"]
+ Output: "fl"
+
+ Input: strs = ["dog","racecar","car"]
+ Output: ""
+ Explanation: There is no common prefix among the input strings.
+ */
+
+ var strs = ["flower","flow","flight"]
+
+func longestCommonPrefix(_ strs: [String]) -> String {
+    return ""
+}
+
+longestCommonPrefix(strs)
--- LeetCode.playground/Pages/28 - ImplementStrStr.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/28 - ImplementStrStr.xcplaygroundpage/Contents.swift
@@ -12,29 +12,22 @@ import Foundation
  */
 
 func strStr(_ haystack: String, _ needle: String) -> Int {
-    if haystack.isEmpty {
-        if needle.isEmpty { return 0 }
-        return -1
-    }
-
     if needle.isEmpty { return 0 }
+    if needle.count > haystack.count { return -1 }
+    
+    var firstIndex = 0
+    var rangeStartIndex = haystack.startIndex
+    var rangeEndIndex = haystack.index(rangeStartIndex, offsetBy: needle.count - 1)
 
-    let needleCount = needle.count
-    var start = 0
-    var end = needleCount - 1
-
-    while end < haystack.count {
-        let startIndex = haystack.index(haystack.startIndex, offsetBy: start)
-        let endIndex = haystack.index(haystack.startIndex, offsetBy: end)
-        let substring = haystack[startIndex ... endIndex]
+    while rangeEndIndex < haystack.endIndex {
+        let substring = haystack[rangeStartIndex ... rangeEndIndex]
         if substring == needle {
-            return start
+            return firstIndex
         }
-
-        start += 1
-        end += 1
+        rangeStartIndex = haystack.index(after: rangeStartIndex)
+        rangeEndIndex = haystack.index(after: rangeEndIndex)
+        firstIndex += 1
     }
-
     return -1
 }
 
