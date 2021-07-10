e0e7dfe0893aa244e59255c220e54f3b28a9d3bd
Andrey Chirkov <achirkof@gmail.com>
2021-07-10 18:36:34 +0300

    Implement StrStr with KMP and time complexity O(n+m)

--- LeetCode.playground/Pages/ImplementStrStr.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/ImplementStrStr.xcplaygroundpage/Contents.swift
@@ -18,18 +18,50 @@ func strStr(_ haystack: String, _ needle: String) -> Int {
     if needle.isEmpty { return 0 }
     if m < n { return -1 }
 
+    // Шаг - 1
+    // Готовим массив по алгоритму Кнута-Морриса-Пратта
+    var p = [0]
+    var i = 1
+    var j = 0
+
+    while i < n {
+        if needle[needle.index(needle.startIndex, offsetBy: j)] == needle[needle.index(needle.startIndex, offsetBy: i)] {
+            p.append(j + 1)
+            i += 1
+            j += 1
+        } else {
+            if j == 0 {
+                p.append(0)
+                i += 1
+            } else {
+                j = p[j - 1]
+            }
+        }
+    }
+
+    // Шаг - 2
+    // С помощью массива проверяем вхождение образа в стоку
+    var k = 0
+    var l = 0
     var index: Int?
 
-    for i in 0 ... m - n {
-        for j in 0 ..< n {
-            let haystackChar = haystack[haystack.index(haystack.startIndex, offsetBy: i + j)]
-            let needleChar = needle[needle.index(needle.startIndex, offsetBy: j)]
-            if haystackChar != needleChar {
+    while l < n, k < m {
+        if haystack[haystack.index(haystack.startIndex, offsetBy: k)] == needle[needle.index(needle.startIndex, offsetBy: l)] {
+            if l == n - 1 {
+                index = k - l
                 break
+            } else {
+                k += 1
+                l += 1
             }
-
-            if j == n - 1 && index == nil {
-                index = i
+        } else if l != 0 {
+            l = p[l - 1]
+        } else {
+            if k == m - 1 {
+                index = -1
+                break
+            } else {
+                k += 1
             }
         }
     }
@@ -45,3 +77,4 @@ strStr("bc", "dec")
 strStr("", "a")
 strStr("a", "a")
 strStr("aaa", "a")
+strStr("mississippi", "issipi")
