41ec728ecec36bb4d14ab89da2188a92b8f56d66
Andrey Chirkov <achirkof@gmail.com>
2021-07-10 18:49:13 +0300

    Implement StrStr with time complexity O(n)

--- LeetCode.playground/Pages/ImplementStrStr.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/ImplementStrStr.xcplaygroundpage/Contents.swift
@@ -12,61 +12,30 @@ import Foundation
  */
 
 func strStr(_ haystack: String, _ needle: String) -> Int {
-    let m = haystack.count
-    let n = needle.count
+    if haystack.isEmpty {
+        if needle.isEmpty { return 0 }
+        return -1
+    }
 
     if needle.isEmpty { return 0 }
-    if m < n { return -1 }
 
-    // Шаг - 1
-    // Готовим массив по алгоритму Кнута-Морриса-Пратта
-    var p = [0]
-    var i = 1
-    var j = 0
+    let needleCount = needle.count
+    var start = 0
+    var end = needleCount - 1
 
-    while i < n {
-        if needle[needle.index(needle.startIndex, offsetBy: j)] == needle[needle.index(needle.startIndex, offsetBy: i)] {
-            p.append(j + 1)
-            i += 1
-            j += 1
-        } else {
-            if j == 0 {
-                p.append(0)
-                i += 1
-            } else {
-                j = p[j - 1]
-            }
+    while end < haystack.count {
+        let startIndex = haystack.index(haystack.startIndex, offsetBy: start)
+        let endIndex = haystack.index(haystack.startIndex, offsetBy: end)
+        let substring = haystack[startIndex ... endIndex]
+        if substring == needle {
+            return start
         }
+
+        start += 1
+        end += 1
     }
 
-    // Шаг - 2
-    // С помощью массива проверяем вхождение образа в стоку
-    var k = 0
-    var l = 0
-    var index: Int?
-
-    while l < n, k < m {
-        if haystack[haystack.index(haystack.startIndex, offsetBy: k)] == needle[needle.index(needle.startIndex, offsetBy: l)] {
-            if l == n - 1 {
-                index = k - l
-                break
-            } else {
-                k += 1
-                l += 1
-            }
-        } else if l != 0 {
-            l = p[l - 1]
-        } else {
-            if k == m - 1 {
-                index = -1
-                break
-            } else {
-                k += 1
-            }
-        }
-    }
-
-    return index ?? -1
+    return -1
 }
 
 strStr("hello", "ll")
